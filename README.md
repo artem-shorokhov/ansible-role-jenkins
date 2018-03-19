2c17b73a59b8a0ec259e9ff2c6e16e0a48c9e3dd
Artem Shorokhov <artem.shorokhov@gmail.com>
2018-03-19 22:05:46 +0200

    Added InSpec tests.

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-# Jenkins master + slave Ansible role
+# Jenkins master + slave Ansible role with InSpec tests run by KitchenCI
 
 ## Requirements
 
 - Virtual Box
 - Vagrant
+- Chef Development Kit
 
 ## Options
 
 - anonymous read access is controlled by `allow_anonymous_read` variable in `roles\jenkins-master\vars\main.yml`
 - additional users could be added via `jenkins_users` variable in `roles\jenkins-master\vars\main.yml`
 - any of Debian or RedHat family OS are supported for Jenkins slave (defined in Vagrantfile)
+
+## Notable commands
+
+### KitchenCI
+
+- `kitchen create` - create virtual machine
+- `kitchen converge` - provision created instance with Ansible playbook
+- `kitchen verify` - run InSpec tests against provisioned instance
+- `kitchen destroy` - destroy virtual machine
+- `kitchen test` - same as running `kitchen destroy` (if instance exists) > `kitchen create` > `kitchen converge` > `kitchen verify` > `kitchen destroy`
+- `kitchen login` - connect to virtual machine via SSH
+
+### Vagrant
+
+- `vagrant up` - create virtual machine and provision it with Ansible playbook
+- `vagrant ssh` - login into virtual machine
---
+++ test/jenkins-master.rb
@@ -0,0 +1,46 @@
+control 'packages' do
+  impact 1.0
+  title 'Required packages should be installed.'
+  describe package('jenkins') do
+    it { should be_installed }
+  end
+end
+
+control 'config-file' do
+  impact 1.0
+  title 'Configuration file should be in place.'
+  describe file('/etc/sysconfig/jenkins') do
+    it { should exist }
+    its('content') { should match JENKINS_JAVA_OPTIONS="-Djenkins.install.runSetupWizard=false" }
+  end
+end
+
+control 'init-scripts' do
+  impact 0.3
+  title 'Groovy init files should be absent.'
+  describe file('/var/lib/jenkins/init.groovy.d/basic-security.groovy') do
+    it { should_not exist }
+  end
+  describe file('/var/lib/jenkins/init.groovy.d/users.groovy') do
+    it { should_not exist }
+  end
+end
+
+control 'jenkins-service' do
+  impact 1.0
+  title 'Jenkins service should be installed/enabled/running.'
+  describe service('jenkins') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+
+control 'port-is-open' do
+  impact 1.0
+  title 'Jenkins service should be installed/enabled/running.'
+  describe port(8080) do
+    it { should be_listening }
+    its('processes') { should include 'java' }
+  end
+end
